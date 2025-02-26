b7dc6b7a4902c242a113aed79cad8f71b3e1add8
LRFranklin-Projects <lukefranklin2006@gmail.com>
2025-02-26 13:46:39 -0500

    Seventh Baseline
    This has added HCimg and MainLogo to the assests folder for use.

--- com_holocards/lib/pages/home.dart
+++ com_holocards/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,14 +8,37 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gotham Ultra',
-                style: TextStyle(
-                  fontFamily: 'GothamUltra',
-                  fontSize: 24,
-                  ),
-                ),
-                backgroundColor: Colors.blue,
-                centerTitle: true,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Holo',
+              style: TextStyle(
+                fontFamily: 'AcLonica',
+                fontSize: 24,
+                color: Color.fromARGB(255, 34, 85, 34),
+              ),
+            ),
+            Text(
+              'Cards',
+              style: TextStyle(
+                fontFamily: 'AcLonica',
+                fontSize: 24,
+                color: const Color.fromARGB(255, 245, 220, 140),
+              ),
+            ),
+          ],
+        ),
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        leading: Container(
+          margin: EdgeInsets.all(10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10)
+          ),
+          child: SvgPicture.asset('assests/icons/MainLogo.svg')
+        ),
       )
     );
   }
