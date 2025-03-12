9de811943ed75763bedb3954c133959cb5d491c8
Payton <payton@pazack.com>
2025-03-12 13:19:38 -0400

    Stashed
    ffff

--- com_holocards/web/Scripts/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu() {
-    const navMenu = document.querySelector('.nav-menu');
-    const hamburger = document.querySelector('.hamburger');
-    navMenu.classList.toggle('active');
-    hamburger.classList.toggle('active');
-}
